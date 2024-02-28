509ac49c1ac41ae431d18fea07d443b35804866a
Adnan Khan <beingadnankhan678@gmail.com>
2024-02-28 07:28:27 +0530

    File Added

---
+++ Pow(Xn).java
@@ -0,0 +1,14 @@
+public class Pow(Xn) {
+    class Solution {
+        public double myPow(double x, int n) {
+            long y=(Math.abs(long)n);
+            double res=1.0;
+    
+            while(y>0){
+                if(y%2==1){
+                    
+                }
+            }
+        }
+    }
+}
